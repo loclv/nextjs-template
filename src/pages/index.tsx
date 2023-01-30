deef64dacfbb73c0c18f1103bffa120318557dcf
loclv <masamonedante@gmail.com>
2023-01-30 17:20:19 +0700

    fix: change mock API URL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,7 @@ if (env) console.log('🚀 ~ env', env);
 
 const getData = async () => {
   try {
-    const response = await fetch(
-      'https://jsonplaceholder.typicode.com/todos/1'
-    );
+    const response = await fetch('/api/hello');
     const data = await response.json();
     console.log(data);
   } catch (error) {
