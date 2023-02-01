20fdea0dcfa0ed5be886fdad6585d53b3906848f
loclv <masamonedante@gmail.com>
2023-02-01 11:10:15 +0700

    feat: add unzip by fflate

--- src/utils/zip.ts
+++ src/utils/zip.ts
@@ -1,5 +1,22 @@
-import { zlibSync } from 'fflate';
+import { zlibSync, unzipSync, Unzipped } from 'fflate';
 
 export const zip = (file: Uint8Array) => {
   return zlibSync(file, { level: 9 });
+};
+
+export const unzip = (
+  file: Uint8Array
+): string | undefined | never | Unzipped => {
+  let unzipFile: undefined | Unzipped;
+
+  try {
+    unzipFile = unzipSync(file);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    console.log('🚀 ~ error', error);
+
+    throw error;
+  }
+
+  return unzipFile;
 };
