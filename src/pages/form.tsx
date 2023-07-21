2a07277f4079ce84045fa78ee68c7498b814925d
loclv <masamonedante@gmail.com>
2023-07-21 17:56:46 +0700

    fix: update type by zod

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -2,16 +2,14 @@ import { FieldErrors, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
-type TForm = {
-  firstName: string;
-};
-
 const schema = z.object({
   firstName: z
     .string()
     .min(1, { message: 'First name is required' })
     .min(8, { message: 'First name must have at least 8 characters' }),
 });
+
+type TForm = z.infer<typeof schema>;
 
 const defaultValues: TForm = {
   firstName: '',
@@ -30,6 +28,7 @@ export default function MyForm() {
 
   const onFormSubmit = (data: TForm) => console.log(data);
   const onErrors = (errors: FieldErrors<TForm>) => console.error(errors);
+  console.log('rerender [MyForm] : ');
 
   return (
     <>
